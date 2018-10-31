2073c9d58d7c6f06aa26d4df1ddd105ff35f5d0b
k8xu <38595657+k8xu@users.noreply.github.com>
2018-10-31 13:31:41 -0400

    Match master app.py

--- gender_novels/deployment/app.py
+++ gender_novels/deployment/app.py
@@ -17,35 +17,7 @@ def render_overview():
     return render_markdown_any('gender_novels_overview', title='Gender in Novels, 1770–1922')
 
 
-<<<<<<< HEAD
-@app.route('/copyright.html')
-def render_copyright():
-    return render_template('copyright.html')
-
-
-@app.route('/corpora.html')
-def render_corpora():
-    return render_template('corpora.html')
-
-
-@app.route('/topic_one.html')
-def render_topic_one():
-    return render_template('topic_one.html')
-
-
-@app.route('/testing_tutorial.html')
-def render_testing_tutorial():
-    return render_template('testing_tutorial.html')
-
-
-@app.route('/test_page.html')
-def render_test_page():
-    return render_template('test_page.html')
-
-
-=======
 # Still necessary because team.html is partly static HTML and partly converted markdown HTML
->>>>>>> upstream/master
 @app.route('/info/team.html')
 def render_team():
     return render_template('team.html')
@@ -88,12 +60,5 @@ def render_no_slash(fn, title=None):
 if __name__ == '__main__':
     # Open a web browser on the landing page
     import webbrowser
-<<<<<<< HEAD
-    # note to self (Xu): correct on server but might try to use variable instead of hard code,
-    # alternative to hardcoding but find later after development is done
-    # note to self (Xu): should be 188.166.88.141:8021
-    webbrowser.open('http://127.0.0.1:8021/', new=2)
-=======
     webbrowser.open('http://127.0.0.1:8021/', new=1)
->>>>>>> upstream/master
     app.run(host='127.0.0.1', port='8021')
